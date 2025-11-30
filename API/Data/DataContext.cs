8115228bb4c41cf8dfda0ed1296c5bbd6809cd2c
DoganBozkurt <doqan228@gmail.com>
2025-11-30 13:56:41 +0300

    Add initial product data seeding for the Product entity

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,4 +7,17 @@ namespace API.Data;
 class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Product> Products => Set<Product>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Product>().HasData(
+
+            new List<Product>
+            {
+                new Product {Id = 1, Name = "IPhone 15", Description = "Telefon açıklaması", Price = 80000, Stock = 12,ImageUrl = "image1.jpg",IsActive =true},
+                new Product {Id = 2, Name = "IPhone 16", Description = "Telefon açıklaması", Price = 95000, Stock = 20,ImageUrl = "image2.jpg",IsActive =true},
+                new Product {Id = 3, Name = "IPhone 16 Pro", Description = "Telefon açıklaması", Price = 100000, Stock = 80,ImageUrl = "image3.jpg",IsActive =false}
+            }
+        );
+    }
 }
---
+++ API/Migrations/20251130105505_TestData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "IsActive", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, "Telefon açıklaması", "image1.jpg", true, "IPhone 15", 80000m, 12 },
+                    { 2, "Telefon açıklaması", "image2.jpg", true, "IPhone 16", 95000m, 20 },
+                    { 3, "Telefon açıklaması", "image3.jpg", false, "IPhone 16 Pro", 100000m, 80 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
